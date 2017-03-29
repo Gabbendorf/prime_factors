6867cc277e04fca7ff1976f83721324b912aba32
Gabriella <gabriellamds@yahoo.it>
2017-03-29 11:50:44 +0100

    written function that checks if the number passed is a prime number

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,8 +1,34 @@
 class PrimeFactors
 
   def get_prime_factors(number)
-    [1, 2]
+    
+  end
+
+  def is_prime?(num)
+    (2..num).each do |i|
+      if num % i == 0 && i < num
+        return false
+      end
+    end
+    true
   end
 
 
 end
+
+
+
+
+
+
+
+
+# i = 2
+# primes = []
+#   while i <= number
+#     if number % i == 0
+#       primes.push(i)
+#     end
+#     i += 1
+#   end
+#   primes
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -1,12 +1,23 @@
-require 'prime_factors'
+require '../lib/prime_factors'
 
-Rspec.describe PrimeFactors do
+RSpec.describe PrimeFactors do
 
   let(:prime_factors) {PrimeFactors.new}
 
-  it "returns an array with 2 and 1" do
-    expect(prime_factors.get_prime_factors(2)).to eq([1, 2])
+  it "returns an array with 2 when passing 2" do
+    expect(prime_factors.get_prime_factors(2)).to eq([2])
   end
 
+  it "returns an array with 5 when passing 5" do
+    expect(prime_factors.get_prime_factors(5)).to eq([5])
+  end
+  #
+  # it "returns an array with 2, 3 when passing 6" do
+  #   expect(prime_factors.get_prime_factors(6)).to eq([2, 3])
+  # end
+  #
+  # it "returns an array with 2, 2 and 3 when passing 12" do
+  #   expect(prime_factors.get_prime_factors(12)).to eq([2, 2, 3])
+  # end
 
 end
