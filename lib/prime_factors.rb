0d5f8ae4416b164a40a42308bc4f1dd67c73d9fb
Gabriella <gabriellamds@yahoo.it>
2017-04-01 17:35:15 +0100

    implemented function get_prime_factors

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,11 +1,17 @@
 class PrimeFactors
 
-  def get_prime_factors(number)
-    factors = []
-    if number >= 2
-      factors.push(number)
+  def get_prime_factors(natural_number)
+    if natural_number <= 1
+      return []
     end
-    factors
+    prime_factors = []
+    (2..natural_number).each do |number|
+      while natural_number % number == 0
+        prime_factors.push(number)
+        natural_number /= number
+      end
+    end
+    prime_factors
   end
 
 end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -16,16 +16,47 @@ RSpec.describe PrimeFactors do
     expect_primes(2, [2])
   end
 
-  # it 'returns array with 3 for 3' do
-  #   expect_primes(3, [3])
-  # end
-  #
-  # it 'returns array with 2,2 for 4' do
-  #   expect_primes(4, [2, 2])
-  # end
+  it 'returns array with 2,2 for 4' do
+    expect_primes(4, [2, 2])
+  end
 
-  # it 'returns array with 3,3 for 9' do
-  #   expect_primes(9, [3, 3])
-  # end
+  it 'returns array with 2,2,2 for 8' do
+    expect_primes(8, [2, 2, 2])
+  end
 
+  it 'returns array with 2,2,2,2,2,2,2,2,2,2 for 1024' do
+    expect_primes(1024, [2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+  end
+
+  it 'returns array with 3 for 3' do
+    expect_primes(3, [3])
+  end
+
+  it 'returns array with 3,3 for 9' do
+    expect_primes(9, [3, 3])
+  end
+
+  it 'returns array with 3,3,3 for 27' do
+    expect_primes(27, [3, 3, 3])
+  end
+
+  it 'returns array with 2,2,3 for 12' do
+    expect_primes(12, [2, 2, 3])
+  end
+
+  it 'returns array with 5 for 5' do
+    expect_primes(5, [5])
+  end
+
+  it 'returns array with 2,3,5 for 30' do
+    expect_primes(30, [2, 3, 5])
+  end
+
+  it 'returns array with 97 for 97' do
+    expect_primes(97, [97])
+  end
+
+  it 'returns array with 2, 2, 2, 3, 7, 11, 97, 97 for 17387832' do
+    expect_primes(17387832, [2, 2, 2, 3, 7, 11, 97, 97])
+  end
 end
