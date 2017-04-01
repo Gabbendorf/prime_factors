dbc92fefffd1e3661bf47dfaa8800261657c15f2
Gabriella <gabriellamds@yahoo.it>
2017-04-01 16:15:54 +0100

    removed function is_prime?

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,39 +1,11 @@
 class PrimeFactors
 
   def get_prime_factors(number)
-   
-  end
-
-  # def return_primes(number)
-  #   if is_prime?(number) && number != 1
-  #     prime
-  #   end
-  # end
-
-  def is_prime?(number)
-    (2..number).each do |i|
-      if number % i == 0 && i < number
-        return false
-      end
+    factors = []
+    if number >= 2
+      factors.push(number)
     end
-    true
+    factors
   end
 
 end
-
-
-
-
-
-
-
-
-# i = 2
-# primes = []
-#   while i <= number
-#     if number % i == 0
-#       primes.push(i)
-#     end
-#     i += 1
-#   end
-#   primes
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -1,23 +1,31 @@
-require '../lib/prime_factors'
+require 'prime_factors'
 
 RSpec.describe PrimeFactors do
 
   let(:prime_factors) {PrimeFactors.new}
 
-  it "returns an empty array when passing 1" do
-    expect(prime_factors.get_prime_factors(1)).to eq([])
+  def expect_primes(number, primes)
+    expect(prime_factors.get_prime_factors(number)).to eq(primes)
   end
 
-  it "returns an array with 2 when passing 2" do
-    expect(prime_factors.get_prime_factors(2)).to eq([2])
+  it 'returns empty array for 1' do
+    expect_primes(1, [])
   end
 
-  it "returns an array with 2, 2 when passing 4" do
-    expect(prime_factors.get_prime_factors(4)).to eq([2, 2])
+  it 'returns array with 2 for 2' do
+    expect_primes(2, [2])
   end
 
-  # it "returns an array with 2, 2 and 3 when passing 12" do
-  #   expect(prime_factors.get_prime_factors(12)).to eq([2, 2, 3])
+  # it 'returns array with 3 for 3' do
+  #   expect_primes(3, [3])
+  # end
+  #
+  # it 'returns array with 2,2 for 4' do
+  #   expect_primes(4, [2, 2])
+  # end
+
+  # it 'returns array with 3,3 for 9' do
+  #   expect_primes(9, [3, 3])
   # end
 
 end
