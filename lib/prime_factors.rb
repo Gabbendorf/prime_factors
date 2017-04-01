8c67d19c1807e04314d7da7e413389440637fd03
Gabriella <gabriellamds@yahoo.it>
2017-04-01 17:36:30 +0100

    deleted condition for 1 (not necessary)

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,9 +1,6 @@
 class PrimeFactors
 
   def get_prime_factors(natural_number)
-    if natural_number <= 1
-      return []
-    end
     prime_factors = []
     (2..natural_number).each do |number|
       while natural_number % number == 0
